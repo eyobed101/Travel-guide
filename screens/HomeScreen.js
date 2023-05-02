f84fa53792e2f2230ce656f79f4cc87c75c369de
eyobed101 <eyocjjazz101@gmail.com>
2023-05-02 23:49:53 +0300

    HomeScreen ended

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,9 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Touchable } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { HeroImage } from '../assets';
-
+import {Image, TouchableOpacity} from 'react-native';
+import * as Animatable from "react-native-animatable"
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -29,16 +30,34 @@ const HomeScreen = () => {
         <Text className="text-[#AD8596] text-[38px] font-bold">Good Moments</Text>
       
       <Text className="text-[#3C6072] text-base">The best camera for hiking and backpacking is a camera that will take 
-        great photos be compact, lightweight, and able to withstand the .</Text>
+        greates photos be compact, lightweight, and able to withstand the .</Text>
       </View>
 
-      <View className="w-[300px] h-[300px] rounded-full bg-[#014D9B] absolute bottom-36 -right-36"></View>
-      <View className="w-[300px] h-[300px] rounded-full bg-[#AD8596] absolute -bottom-20 -left-36"></View>
+      <View className="w-[200px] h-[200px] rounded-full bg-[#014D9B] absolute bottom-60 -right-20"></View>
+      <View className="w-[200px] h-[200px] rounded-full bg-[#AD8596] absolute -bottom-10 -left-20"></View>
 
           {/*Image container  */}
           <View className = "flex-1 relative items-center justify-center">
-          <Image source={{uri: '../assets/mainHero.png'}} />
+          <Animatable.Image 
+          animation={"fadeIn"}
+          easing="ease-in-out"
+          source={HeroImage}  className="mt-12 w-[280px] h-[400px] object-cover"/>
+          
+          <View className="absolute bottom-20 w-24 h-24  border-l-2 border-r-2 border-t-4  border-[#014D9B] rounded-[999px]  items-center justify-center">
+          <TouchableOpacity>
+            <Animatable.View 
+            animation={"pulse"}
+            easing="ease-in-out"
+            iterationCount={"infinite"}
+            className="w-20 h-20 items-center justify-center rounded-full bg-[#014D9B]">
+              <Text className="text-white text-[25px] font-bold">Go</Text>
+            </Animatable.View>
+            </TouchableOpacity>
           </View>
+          
+          </View>
+
+          
     </SafeAreaView>
   )
 }
