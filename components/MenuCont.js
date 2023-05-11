ee49ffb2986f2d44df4f38404c63a04e762c4e80
eyobed101 <eyocjjazz101@gmail.com>
2023-05-11 21:14:31 +0300

    Edited

--- components/MenuCont.js
+++ components/MenuCont.js
@@ -4,16 +4,18 @@ import React from 'react'
 const MenuCont = ({title, imageSrc, type, setType}) => {
     const handlePress =() =>{
         setType(title.toLowerCase())
+        
     }
   return (
+
     <TouchableOpacity className="items-center justify-center space-y-2" onPress={handlePress}>
-      <View className={`w-24 h-24 items-center justify-center shadow-sm rounded-full ${type === title.toLowerCase() ? "bg-gray-200" : " "}`}>
+      <View className={`w-24 h-24 p-2 items-center justify-center shadow-sm rounded-full ${type === title.toLowerCase() ? "bg-gray-200" : " "}`}>
         <Image 
             source = {imageSrc}
             className="w-full h-full object-cover"
         />  
       </View>
-      <Text className="text-[#00bcc9] text-xl font-semibold">{title}</Text>
+      <Text className="text-[#014D9B] text-md font-semibold">{title}</Text>
     </TouchableOpacity>
   )
 }
